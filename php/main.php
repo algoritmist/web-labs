2ed6958db0211dc2fab0892a739e1b4d354c5fbb
Viacheslav Baranov <barash.2018@yandex.ru>
2022-10-27 10:34:59 +0300

    added 401 bad response if validation failed

--- php/main.php
+++ php/main.php
@@ -1,25 +1,6 @@
 <?php
 function checkIn($x, $y, $r)
 {
-    $xMin = -4;
-    $xMax = 4;
-    $yMin = -3;
-    $yMax = 5;
-    $rMin = 1;
-    $rMax = 5;
-
-    if(!($xMin <= $x and $x <= $xMax)){
-        return false;
-    }
-
-    if(!($yMin <= $y and $y <= $yMax)){
-        return false;
-    }
-
-    if(!($rMin <= $r and $r <= $rMax)){
-        return false;
-    }
-
     if ($x >= 0 and $y >= 0) {
         return $x <= $r and 2 * ($x + $y) <= $r;
     }
@@ -29,13 +10,36 @@ function checkIn($x, $y, $r)
     return $y <= 0 and $x * $x + $y * $y <= $r;
 }
 
-$x = $_POST['x'];
-$y = $_POST['y'];
-$r = $_POST['r'];
+$x = (int) $_POST['x'];
+$y = (int) $_POST['y'];
+$r = (int) $_POST['r'];
 $timezoneOffset = $_POST['timezone'];
 
 $start = date("H:i:s", time() - $timezoneOffset * 60);
 $time_start = microtime(true);
+
+$xMin = -4;
+$xMax = 4;
+$yMin = -3;
+$yMax = 5;
+$rMin = 1;
+$rMax = 5;
+
+if(!($xMin <= $x and $x <= $xMax)){
+    header('HTTP/1.1 401 Bad Request', true, 401);
+    return;
+}
+
+if(!($yMin <= $y and $y <= $yMax)){
+    header('HTTP/1.1 401 Bad Request', true, 401);
+    return;
+}
+
+if(!($rMin <= $r and $r <= $rMax)){
+    header('HTTP/1.1 401 Bad Request', true, 401);
+    return;
+}
+
 $result = checkIn($x, $y, $r) ? "Попадание" : "Промах";
 $time_end = microtime(true);
 $execution_time = ($time_end - $time_start);
